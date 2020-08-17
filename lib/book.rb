9d86382d9df1eff4d95e70b19a4400a9712fff16
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 20:21:55 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  GENRES = []
 
   def initialize(title)
     @title = title
+    GENRES << :genre
   end
 
   def turn_page
