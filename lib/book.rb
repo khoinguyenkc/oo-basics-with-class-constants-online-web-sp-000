67aac0cf7036fa40b21c24e53e9555343112a8e5
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 20:24:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,19 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count,
+  attr_reader :title :genre
   GENRES = []
 
   def initialize(title)
     @title = title
-    GENRES << :genre
   end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << @genre
+    #at first, before genre is defined, this will be emtpy
+    #but i guess whne its defined, then this is updated...
+    #i'm not sure the logic behind. reference vs copy
+
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
