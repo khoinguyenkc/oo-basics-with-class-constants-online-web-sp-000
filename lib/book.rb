d7d62aeda07c39146220384ce740f6bf19c7ab29
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 20:27:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count,
-  attr_reader :title :genre
+  attr_reader :title, :genre
   GENRES = []
 
   def initialize(title)
@@ -16,7 +16,7 @@ class Book
     #it doesnt happen!! i still am not sure the exact details of reference vs copy
   end
 
-  
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
